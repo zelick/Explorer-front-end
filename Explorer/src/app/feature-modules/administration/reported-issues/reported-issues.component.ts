32d727b651f7c87d944dbe0388d5cef9f2d7a190
Dušan <duci.sudjic@gmail.com>
2023-10-17 22:35:18 +0200

    fix: Date formats adjusted.

--- Explorer/src/app/feature-modules/administration/reported-issues/reported-issues.component.ts
+++ Explorer/src/app/feature-modules/administration/reported-issues/reported-issues.component.ts
@@ -2,13 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { AdministrationService } from '../administration.service';
 import { ReportedIssue } from '../model/reported-issue.model';
 import { PagedResults } from 'src/app/shared/model/paged-results.model';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'xp-reported-issues',
   templateUrl: './reported-issues.component.html',
-  styleUrls: ['./reported-issues.component.css']
+  styleUrls: ['./reported-issues.component.css'],
+  providers: [DatePipe]
 })
 export class ReportedIssuesComponent implements OnInit{
+
   reportedIssues: ReportedIssue[] = [];
   shouldRenderAllAgain: boolean = false;
 
@@ -17,7 +21,6 @@ export class ReportedIssuesComponent implements OnInit{
   ngOnInit(): void{
     this.getReportedIssues();
   }
-
   getReportedIssues(): void{
     this.service.getReportedIssues().subscribe({
       next:(result:PagedResults<ReportedIssue>)=>{
