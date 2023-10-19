e611fb316a0db558072001b12249d2f9f4834b06
Matija Maksimović <116630740+MatijaMax@users.noreply.github.com>
2023-10-19 22:30:44 +0200

    feat: Completed frontend.

--- Explorer/src/app/infrastructure/routing/app-routing.module.ts
+++ Explorer/src/app/infrastructure/routing/app-routing.module.ts
@@ -5,12 +5,16 @@ import { LoginComponent } from '../auth/login/login.component';
 import { EquipmentComponent } from 'src/app/feature-modules/administration/equipment/equipment.component';
 import { AuthGuard } from '../auth/auth.guard';
 import { RegistrationComponent } from '../auth/registration/registration.component';
+import { PreferenceComponent } from 'src/app/feature-modules/marketplace/preference/preference/preference.component';
+import { PreferenceFormComponent } from 'src/app/feature-modules/marketplace/preference-form/preference-form/preference-form.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegistrationComponent},
-  {path: 'equipment', component: EquipmentComponent, canActivate: [AuthGuard],}
+  {path: 'equipment', component: EquipmentComponent, canActivate: [AuthGuard]},
+  {path: 'preference-form', component: PreferenceFormComponent, canActivate:[AuthGuard]},
+  {path: 'preference', component: PreferenceComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
