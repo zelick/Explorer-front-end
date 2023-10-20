fff43f53258e37252e02002f24155264987761c0
nina <ninabu16@gmail.com>
2023-10-20 22:09:11 +0200

    feat: add components for blog creation

--- Explorer/src/app/feature-modules/blog/blog.module.ts
+++ Explorer/src/app/feature-modules/blog/blog.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { BlogPostFormComponent } from './blog-post-form/blog-post-form.component';
+import { MaterialModule } from 'src/app/infrastructure/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BlogPostComponent,
+    BlogPostFormComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    MatSelectModule
+  ],
+  exports: [
+    BlogPostComponent,
+    BlogPostFormComponent
   ]
 })
 export class BlogModule { }
---
+++ Explorer/src/app/feature-modules/blog/blog.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BlogPost } from './model/blog-post.model';
+import { environment } from 'src/env/environment';
+import { Observable } from 'rxjs';
+import { PagedResults } from 'src/app/shared/model/paged-results.model';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogService {
+  userId: number;
+
+  constructor(private http: HttpClient, private authService: AuthService) { 
+    this.userId = authService.user$.value.id;
+  }
+
+  getBlogPosts(): Observable<PagedResults<BlogPost>> {
+    return this.http.get<PagedResults<BlogPost>>(environment.apiHost + `blogging/blog-post/user/${this.userId}`)
+  }
+
+  addBlogPost(blogPost: BlogPost): Observable<BlogPost> {
+    return this.http.post<BlogPost>(environment.apiHost + 'blogging/blog-post', blogPost);
+  }
+
+  deleteBlogPost(id: number): Observable<BlogPost> {
+    return this.http.delete<BlogPost>(environment.apiHost + `blogging/blog-post/${id}`);
+  }
+
+  updateBlogPost(blogPost: BlogPost): Observable<BlogPost> {
+    return this.http.put<BlogPost>(environment.apiHost + `blogging/blog-post/${blogPost.id}`, blogPost);
+  }
+
+  closeBlog(id: number): Observable<BlogPost> {
+    return this.http.patch<BlogPost>(environment.apiHost + `blogging/blog-post/${id}/close`, null);
+  }
+}
