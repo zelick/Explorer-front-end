51c1a727cd2db12c5ecf88a13280e523acd85c10
draskonovakovic <116906185+draskonovakovic@users.noreply.github.com>
2023-10-23 17:31:51 +0200

    front done

--- Explorer/src/app/feature-modules/layout/profile-administration/profile-administration.component.ts
+++ Explorer/src/app/feature-modules/layout/profile-administration/profile-administration.component.ts
@@ -43,7 +43,7 @@ export class ProfileAdministrationComponent implements OnInit{
         biography: user.biography,
         motto: user.motto,
       });
-
+      
       this.selectedImage = user.profilePictureUrl;
     });
   }
