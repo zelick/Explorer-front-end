ab6c5eff7fe4dd48767148ca882f9987e20f0884
Dušan <duci.sudjic@gmail.com>
2023-12-01 20:16:10 +0100

    feat: private tour model created

---
+++ Explorer/src/app/shared/model/private-tour.model.ts
@@ -0,0 +1,8 @@
+import { PublicCheckpoint } from "src/app/feature-modules/tour-execution/model/public_checkpoint.model";
+
+export interface PrivateTour{
+    id: number;
+    touristId: number;
+    checkPoints: PublicCheckpoint[];
+    name:string;
+}
