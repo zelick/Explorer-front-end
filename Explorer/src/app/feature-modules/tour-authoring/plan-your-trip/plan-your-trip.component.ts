ac985ee7b1b0fd0fd0911c819e2bc91cf3cb9b41
Dušan <duci.sudjic@gmail.com>
2023-12-01 19:36:20 +0100

    feat: binding data from back to recommended tours

--- Explorer/src/app/feature-modules/tour-authoring/plan-your-trip/plan-your-trip.component.ts
+++ Explorer/src/app/feature-modules/tour-authoring/plan-your-trip/plan-your-trip.component.ts
@@ -5,6 +5,7 @@ import { TourAuthoringService } from '../tour-authoring.service';
 import { MapComponent } from 'src/app/shared/map/map.component';
 import { Tour } from '../model/tour.model';
 import { Router } from '@angular/router';
+import { PublicTour } from '../../marketplace/model/public-tour.model';
 
 @Component({
   selector: 'xp-plan-your-trip',
@@ -20,89 +21,97 @@ export class PlanYourTripComponent implements OnInit, AfterViewInit{
   selectedLatitude: number;
   selectedLongitude: number;
   i: number = 0;
-  tours: Tour[] = [
-    { 
-      id: 1, 
-      authorId: 2, 
-      name: "Brutal experience", 
-      demandignessLevel: "Easy", 
-      tags: ["genius", "beautiful"], 
-      description: "Beautiful walk alongside the river which turns into a great adventure at midnight. Let's have fun", 
-      price: 120, 
-      status: "Published", 
-      tourRatings: [
-        { id: 1, tourId: 1, tourDate: new Date(), touristId: 3, rating: 5, creationDate: new Date() }, 
-        { id: 2, tourId: 1, tourDate: new Date(), touristId: 3, rating: 4, creationDate: new Date() }
-      ], 
-      tourTimes: [], 
-      equipment: [], 
-      checkpoints: [{tourId:1, authorId:2, longitude:12.0, latitude:13.9, name:'cd', description: 'yy', pictures:['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCC5RVYslWxQeeNZCsKTbV0Slq4rYZ4E96kw&usqp=CAU'], requiredTimeInSeconds:1200, currentPicture:0, currentPointPicture: 0, viewSecretMessage:'mmm', visibleSecret:true, showedPicture:'', showedPointPicture:''}]
-    },
-    { 
-      id: 2, 
-      authorId: 2, 
-      name: "Mountain Expedition", 
-      demandignessLevel: "Moderate", 
-      tags: ["adventure", "scenic"], 
-      description: "Explore the breathtaking landscapes of the mountains. A moderate-level expedition for adventure enthusiasts.", 
-      price: 150, 
-      status: "Published", 
-      tourRatings: [
-        { id: 1, tourId: 2, tourDate: new Date(), touristId: 3, rating: 2, creationDate: new Date() }, 
-        { id: 2, tourId: 2, tourDate: new Date(), touristId: 3, rating: 4, creationDate: new Date() }
-      ], 
-      tourTimes: [], 
-      equipment: [], 
-      checkpoints: [{tourId:1, authorId:2, longitude:12.0, latitude:13.9, name:'cd', description: 'yy', pictures:['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR94u0EyhIYQ35WVzV0LSlxZ0Ozv9tMqfzewA&usqp=CAU'], requiredTimeInSeconds:1200, currentPicture:0, currentPointPicture: 0, viewSecretMessage:'mmm', visibleSecret:true, showedPicture:'', showedPointPicture:''}]
-    },
-    { 
-      id: 3, 
-      authorId: 2, 
-      name: "City Bike Tour", 
-      demandignessLevel: "Easy", 
-      tags: ["urban", "cycling"], 
-      description: "Discover the city on a bike. An easy and enjoyable tour through urban landscapes and iconic landmarks.", 
-      price: 80, 
-      status: "Published", 
-      tourRatings: [
-        { id: 1, tourId: 3, tourDate: new Date(), touristId: 3, rating: 5, creationDate: new Date() }, 
-        { id: 2, tourId: 3, tourDate: new Date(), touristId: 3, rating: 2, creationDate: new Date() }
-      ], 
-      tourTimes: [], 
-      equipment: [], 
-      checkpoints: [{tourId:1, authorId:2, longitude:12.0, latitude:13.9, name:'cd', description: 'yy', pictures:['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ904w-SqHNLprXg4hU5fPoBHMjpWl5dpg0Zg&usqp=CAU'], requiredTimeInSeconds:1200, currentPicture:0, currentPointPicture: 0, viewSecretMessage:'mmm', visibleSecret:true, showedPicture:'', showedPointPicture:''}]
-    },
-    { 
-      id: 4, 
-      authorId: 2, 
-      name: "Jungle Adventure", 
-      demandignessLevel: "Challenging", 
-      tags: ["wildlife", "jungle"], 
-      description: "Embark on a challenging journey through dense jungles. Experience the thrill of wildlife and nature.", 
-      price: 200, 
-      status: "Published", 
-      tourRatings: [], 
-      tourTimes: [], 
-      equipment: [], 
-      checkpoints: [{tourId:1, authorId:2, longitude:12.0, latitude:13.9, name:'cd', description: 'yy', pictures:['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGG3PlQB2yqO1_AmQSR8bgcczzjpOLQ8cy2A&usqp=CAU'], requiredTimeInSeconds:1200, currentPicture:0, currentPointPicture: 0, viewSecretMessage:'mmm', visibleSecret:true, showedPicture:'', showedPointPicture:''}]
-    },
-    { 
-      id: 5, 
-      authorId: 2, 
-      name: "Historical Walking Tour", 
-      demandignessLevel: "Easy", 
-      tags: ["history", "culture"], 
-      description: "Take a stroll through historical sites and learn about the rich cultural heritage of the city.", 
-      price: 100, 
-      status: "Published", 
-      tourRatings: [], 
-      tourTimes: [], 
-      equipment: [], 
-      checkpoints: [{tourId:1, authorId:2, longitude:12.0, latitude:13.9, name:'cd', description: 'yy', pictures:['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQoQuVACa7v79DCJeGHbGh5D_RfSxcJiXCtA&usqp=CAU'], requiredTimeInSeconds:1200, currentPicture:0, currentPointPicture: 0, viewSecretMessage:'mmm', visibleSecret:true, showedPicture:'', showedPointPicture:''}]
-    }]
+  tours: PublicTour[] = [
+    // { 
+    //   id: 1, 
+    //   authorId: 2, 
+    //   name: "Brutal experience", 
+    //   demandignessLevel: "Easy", 
+    //   tags: ["genius", "beautiful"], 
+    //   description: "Beautiful walk alongside the river which turns into a great adventure at midnight. Let's have fun", 
+    //   price: 120, 
+    //   status: "Published", 
+    //   tourRatings: [
+    //     { id: 1, tourId: 1, tourDate: new Date(), touristId: 3, rating: 5, creationDate: new Date() }, 
+    //     { id: 2, tourId: 1, tourDate: new Date(), touristId: 3, rating: 4, creationDate: new Date() }
+    //   ], 
+    //   tourTimes: [], 
+    //   equipment: [], 
+    //   checkpoints: [{tourId:1, authorId:2, longitude:12.0, latitude:13.9, name:'cd', description: 'yy', pictures:['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCC5RVYslWxQeeNZCsKTbV0Slq4rYZ4E96kw&usqp=CAU'], requiredTimeInSeconds:1200, currentPicture:0, currentPointPicture: 0, viewSecretMessage:'mmm', visibleSecret:true, showedPicture:'', showedPointPicture:''}]
+    // },
+    // { 
+    //   id: 2, 
+    //   authorId: 2, 
+    //   name: "Mountain Expedition", 
+    //   demandignessLevel: "Moderate", 
+    //   tags: ["adventure", "scenic"], 
+    //   description: "Explore the breathtaking landscapes of the mountains. A moderate-level expedition for adventure enthusiasts.", 
+    //   price: 150, 
+    //   status: "Published", 
+    //   tourRatings: [
+    //     { id: 1, tourId: 2, tourDate: new Date(), touristId: 3, rating: 2, creationDate: new Date() }, 
+    //     { id: 2, tourId: 2, tourDate: new Date(), touristId: 3, rating: 4, creationDate: new Date() }
+    //   ], 
+    //   tourTimes: [], 
+    //   equipment: [], 
+    //   checkpoints: [{tourId:1, authorId:2, longitude:12.0, latitude:13.9, name:'cd', description: 'yy', pictures:['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR94u0EyhIYQ35WVzV0LSlxZ0Ozv9tMqfzewA&usqp=CAU'], requiredTimeInSeconds:1200, currentPicture:0, currentPointPicture: 0, viewSecretMessage:'mmm', visibleSecret:true, showedPicture:'', showedPointPicture:''}]
+    // },
+    // { 
+    //   id: 3, 
+    //   authorId: 2, 
+    //   name: "City Bike Tour", 
+    //   demandignessLevel: "Easy", 
+    //   tags: ["urban", "cycling"], 
+    //   description: "Discover the city on a bike. An easy and enjoyable tour through urban landscapes and iconic landmarks.", 
+    //   price: 80, 
+    //   status: "Published", 
+    //   tourRatings: [
+    //     { id: 1, tourId: 3, tourDate: new Date(), touristId: 3, rating: 5, creationDate: new Date() }, 
+    //     { id: 2, tourId: 3, tourDate: new Date(), touristId: 3, rating: 2, creationDate: new Date() }
+    //   ], 
+    //   tourTimes: [], 
+    //   equipment: [], 
+    //   checkpoints: [{tourId:1, authorId:2, longitude:12.0, latitude:13.9, name:'cd', description: 'yy', pictures:['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ904w-SqHNLprXg4hU5fPoBHMjpWl5dpg0Zg&usqp=CAU'], requiredTimeInSeconds:1200, currentPicture:0, currentPointPicture: 0, viewSecretMessage:'mmm', visibleSecret:true, showedPicture:'', showedPointPicture:''}]
+    // },
+    // { 
+    //   id: 4, 
+    //   authorId: 2, 
+    //   name: "Jungle Adventure", 
+    //   demandignessLevel: "Challenging", 
+    //   tags: ["wildlife", "jungle"], 
+    //   description: "Embark on a challenging journey through dense jungles. Experience the thrill of wildlife and nature.", 
+    //   price: 200, 
+    //   status: "Published", 
+    //   tourRatings: [], 
+    //   tourTimes: [], 
+    //   equipment: [], 
+    //   checkpoints: [{tourId:1, authorId:2, longitude:12.0, latitude:13.9, name:'cd', description: 'yy', pictures:['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGG3PlQB2yqO1_AmQSR8bgcczzjpOLQ8cy2A&usqp=CAU'], requiredTimeInSeconds:1200, currentPicture:0, currentPointPicture: 0, viewSecretMessage:'mmm', visibleSecret:true, showedPicture:'', showedPointPicture:''}]
+    // },
+    // { 
+    //   id: 5, 
+    //   authorId: 2, 
+    //   name: "Historical Walking Tour", 
+    //   demandignessLevel: "Easy", 
+    //   tags: ["history", "culture"], 
+    //   description: "Take a stroll through historical sites and learn about the rich cultural heritage of the city.", 
+    //   price: 100, 
+    //   status: "Published", 
+    //   tourRatings: [], 
+    //   tourTimes: [], 
+    //   equipment: [], 
+    //   checkpoints: [{tourId:1, authorId:2, longitude:12.0, latitude:13.9, name:'cd', description: 'yy', pictures:['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQoQuVACa7v79DCJeGHbGh5D_RfSxcJiXCtA&usqp=CAU'], requiredTimeInSeconds:1200, currentPicture:0, currentPointPicture: 0, viewSecretMessage:'mmm', visibleSecret:true, showedPicture:'', showedPointPicture:''}]
+    // }
+  ]
   
-  constructor(private mapService: MapService, private tourAuthoringService: TourAuthoringService, private router: Router){
+  constructor(private service: TourAuthoringService,private mapService: MapService, private tourAuthoringService: TourAuthoringService, private router: Router){
 
+  }
+  updateTours(){
+    this.service.getToursWithPublicCheckpoints(this.selectedCheckpoints).subscribe({
+      next: (result)=>{
+        this.tours = result;
+      }
+    });
   }
   ngAfterViewInit(): void {
     if(this.selectedCheckpoints.length > 0)
@@ -125,6 +134,7 @@ export class PlanYourTripComponent implements OnInit, AfterViewInit{
   }
   addCheckpoint(ch:PublicCheckpoint){
     this.selectedCheckpoints.push(ch);
+    this.updateTours();
     let coords: [{lat: number, lon: number, picture: string, name: string, desc: string}] = [{lat: ch.latitude, lon: ch.longitude, picture: ch.pictures[0], name:ch.name, desc: ch.description}];
     this.mapComponent.addPublicCheckpoints(coords);
   }
@@ -133,6 +143,7 @@ export class PlanYourTripComponent implements OnInit, AfterViewInit{
     if (index !== -1) {
       this.selectedCheckpoints.splice(index, 1);
     }
+    this.updateTours();
   }
   addPublicCheckpointsOnMap(): void{
     if(this.selectedCheckpoints)
@@ -180,20 +191,11 @@ export class PlanYourTripComponent implements OnInit, AfterViewInit{
 
 
 
-  selectTour(tour:Tour){
+  selectTour(tour:PublicTour){
       if(tour.id){
         this.router.navigate(['/tour-overview-details', tour.id]);
       }
     }
-  averageGrade(tour: Tour){
-    var sum = 0;
-    var count = 0;
-    for(let g of tour.tourRatings){
-      sum += g.rating;
-      count ++;
-    }
-    return parseFloat((sum/count).toFixed(1)).toFixed(1);
-  }
   swipeRight() {
     const cardWidth = document.querySelector('.tour-card')?.clientWidth || 0;
     const container = document.querySelector('.tour-cards-container');
