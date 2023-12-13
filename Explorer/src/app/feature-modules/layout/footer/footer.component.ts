8ad4a7a26532e08acf1da24c402f0cdb6d0787fa
draskonovakovic <116906185+draskonovakovic@users.noreply.github.com>
2023-12-13 23:32:24 +0100

    feat: administrator home page footer updated

--- Explorer/src/app/feature-modules/layout/footer/footer.component.ts
+++ Explorer/src/app/feature-modules/layout/footer/footer.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
+import { User } from 'src/app/infrastructure/auth/model/user.model';
 
 @Component({
   selector: 'xp-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  user: User | undefined;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe(user => {
+      this.user = user;
+    });
+  }
 }
