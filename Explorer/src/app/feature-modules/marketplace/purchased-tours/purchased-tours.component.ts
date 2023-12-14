0a54843d14320882562c9cda2bd7f8a9c48b5fc8
Anastasija <anastasijamarsenic@gmail.com>
2023-12-14 13:51:52 +0100

    feat: improve purchased tours ui

--- Explorer/src/app/feature-modules/marketplace/purchased-tours/purchased-tours.component.ts
+++ Explorer/src/app/feature-modules/marketplace/purchased-tours/purchased-tours.component.ts
@@ -1,11 +1,9 @@
-import { Component, NgIterable, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 import { User } from 'src/app/infrastructure/auth/model/user.model';
-import { Tour } from '../../tour-authoring/model/tour.model';
 import { MarketplaceService } from '../marketplace.service';
-import { TourAuthoringService } from '../../tour-authoring/tour-authoring.service';
-import { Router } from '@angular/router';
 import { PurchasedTourPreview } from '../../tour-execution/model/purchased_tour_preview.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'xp-purchased-tours',
@@ -17,7 +15,7 @@ export class PurchasedToursComponent implements OnInit {
   user: User;
   purchasedTours: PurchasedTourPreview[] = [];
 
-  constructor(private service: MarketplaceService, private tourService: TourAuthoringService, private authService: AuthService, private router: Router) { }
+  constructor(private service: MarketplaceService, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.authService.user$.subscribe(user => {
@@ -31,11 +29,22 @@ export class PurchasedToursComponent implements OnInit {
       error: () => {
       }
     });
-
   }
 
-  openDetails(tour: PurchasedTourPreview):void{
+  openDetails(tour: PurchasedTourPreview): void {
     this.router.navigate([`purchased-tours-details/${tour.id}`]);
   }
 
+  getDemandColor(demandLevel: string): string {
+    switch (demandLevel.toLowerCase()) {
+      case 'easy':
+        return 'green';
+      case 'medium':
+        return 'orange';
+      case 'hard':
+        return 'red';
+      default:
+        return 'black';
+    }
+  }
 }
